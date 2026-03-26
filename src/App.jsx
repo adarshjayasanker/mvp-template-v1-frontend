b60042a0ad2de45f742f4d68980cb4e6cdb1637a
Adarsh Jayasanker <adarshcodes1@gmail.com>
2026-03-26 22:42:06 +0530

    fvda

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import Waitlist from './pages/Waitlist.js';
-import Tool from './pages/Tool.js';
+import {Routes, Route} from 'react-router-dom';
+import Waitlist from './pages/Waitlist.jsx';
+import Tool from './pages/Tool.jsx';
 
 const App = () => {
   return(
-    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Waitlist/>}/>
         <Route path="/tool" element={<Tool/>}/>
       </Routes>
-    </BrowserRouter>
   )
 };
 
